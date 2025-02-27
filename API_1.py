78a21e8c04299ef545fb868a94b5e028329f7e3e
nathanvdev <nathanvaldez413@gmail.com>
2025-02-27 00:22:16 -0600

    Credenciales para usuario IAM

--- API_1.py
+++ API_1.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/check', methods=['GET'])
 def check_status():
@@ -15,4 +17,4 @@ def get_info():
     })
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
--- API_2.js
+++ API_2.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get("/check", (req, res) => {
+    console.log("endpoint check");
     res.status(200).json({ message: "OK" });
 });
 
 app.get("/info", (req, res) => {
+    console.log("endpoint info");
     res.json({
         Instancia: "Maquina 2 - Api 2",
         Curso: "Seminario de Sistemas 1 A",
@@ -17,4 +19,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
